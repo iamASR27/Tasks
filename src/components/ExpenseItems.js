2409a0d9ea2a196ad55244f84f6f3105b9755c60
iamASR27 <ramardeep1997@gmail.com>
2023-06-22 21:50:26 +0530

    used props

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,28 +1,20 @@
 import './ExpenseItems.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 5, 21);
-  const expenseTitle = "Movies";
-  const LocationOfExpenditure = "Bhopal";
-  const expenseAmount = 200;
-
+function ExpenseItem(props) {
+ 
 
   return (
     <div className="expense-item">
-      {/* <h2>Expense List</h2> */}
-      <div className="date">{expenseDate.toLocaleString()}</div>
+      
+      <div className="date">{props.date.toLocaleDateString()}</div>
       <div className="expense-title">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-location">
-        <h2>{LocationOfExpenditure}</h2>
+        <h2>{props.location}</h2>
       </div>
-      <div className="expense-amount">Rs.{expenseAmount}</div>
-      {/* <ul className="item-list">
-        <li>Food Rs.10</li>
-        <li>Petrol Rs.100</li>
-        <li>Movies Rs.200</li>
-      </ul> */}
+      <div className="expense-amount">Rs. {props.amount}</div>
+      
     </div>
   );
 }
