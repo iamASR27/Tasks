bb9485c3cd51da934f55b53b3b607099a5e9fecb
iamASR27 <ramardeep1997@gmail.com>
2023-06-22 21:49:54 +0530

    looping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import ExpenseItem from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "New Delhi",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 79900.49,
+      date: new Date(2021, 2, 12),
+      location: "Pune",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 2940.67,
+      date: new Date(2021, 2, 28),
+      location: "Bhopal",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 4500,
+      date: new Date(2021, 5, 12),
+      location: "Mumbai",
+    },
+  ];
+
   return (
     <div>
-      {/* <h2>Let's get started!</h2> */}
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
-}
+      }
 
 export default App;
