0c4349806ccadf5c5f7ac15d285ffee741573a09
iamASR27 <ramardeep1997@gmail.com>
2023-06-22 20:26:51 +0530

    added location variable

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,28 @@
+import './ExpenseItems.css';
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 5, 21);
+  const expenseTitle = "Movies";
+  const LocationOfExpenditure = "Bhopal";
+  const expenseAmount = 200;
+
+
   return (
-    <div>
-      <h2>Expense Items</h2>
-      <ul>
+    <div className="expense-item">
+      {/* <h2>Expense List</h2> */}
+      <div className="date">{expenseDate.toLocaleString()}</div>
+      <div className="expense-title">
+        <h2>{expenseTitle}</h2>
+      </div>
+      <div className="expense-location">
+        <h2>{LocationOfExpenditure}</h2>
+      </div>
+      <div className="expense-amount">Rs.{expenseAmount}</div>
+      {/* <ul className="item-list">
         <li>Food Rs.10</li>
         <li>Petrol Rs.100</li>
         <li>Movies Rs.200</li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
