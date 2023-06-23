d00359992eaa0207547b82b863c9a5b57d23583d
iamASR27 <ramardeep1997@gmail.com>
2023-06-23 20:56:39 +0530

    expenseDetails added

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,20 +1,18 @@
-import './ExpenseItems.css';
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import "./ExpenseItems.css";
 
 function ExpenseItem(props) {
- 
-
   return (
     <div className="expense-item">
-      
-      <div className="date">{props.date.toLocaleDateString()}</div>
-      <div className="expense-title">
-        <h2>{props.title}</h2>
-      </div>
-      <div className="expense-location">
-        <h2>{props.location}</h2>
-      </div>
-      <div className="expense-amount">Rs. {props.amount}</div>
-      
+      <ExpenseDate date={props.date} />
+     
+      <ExpenseDetails
+        amount={props.amount}
+        title={props.title}
+        location={props.location}
+      />
+     
     </div>
   );
 }
