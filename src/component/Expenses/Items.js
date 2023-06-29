34c1a5966a5ea238ffbc50f387c6e3422b73a758
iamASR27 <ramardeep1997@gmail.com>
2023-06-29 22:12:35 +0530

    used State concept, added edit button

--- src/component/Expenses/Items.js
+++ src/component/Expenses/Items.js
@@ -1,26 +1,34 @@
+import React, { useState } from 'react';
 import Card from "../UI/Card";
 import ExDate from "./Date";
 import Exdetails from "./Details";
 import './Items.css'
 
 const ExItems = (props) => {
+  const [newTitle, setNewTitle] = useState(props.title);
+  const[newAmount, setNewAmount] = useState(props.amount);
   const clickHandler = () => {
-    console.log("Clicked!");
+    setNewTitle("Updated!");
+    console.log(newTitle);
   }
   const deleteExpense = () => {
     const cardElement = document.getElementsByClassName("ex-items");
     cardElement[0].remove();
   }
+  const editAmount = () => {
+    setNewAmount("100");
+  }
   return (
     <Card className="ex-items">
       <ExDate date={props.date} className="ex-date"/>
       <Exdetails
-        title={props.title}
-        amount={props.amount}
+        title={newTitle}
+        amount={newAmount}
         location={props.location}
       />
       <button onClick={clickHandler}>Change Title</button>
       <button onClick={deleteExpense}>Delete</button>
+      <button onClick={editAmount}>Edit</button>
     </Card>
   );
 };
