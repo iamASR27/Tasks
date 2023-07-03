1f5a7a0c75f748f586835ea653be1cf74a0e8807
iamASR27 <ramardeep1997@gmail.com>
2023-07-03 21:20:59 +0530

    added filter which doesnt work

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 import ExpenseItems from "./ExpenseItems";
-import Card from '../UI/Card';
-
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.expenses.map((expense) => {
         return (
           <ExpenseItems
@@ -19,6 +29,6 @@ const Expenses = (props) => {
       })}
     </Card>
   );
-}
+};
 
 export default Expenses;
